59f6847746e9e3df92418d9848af1974aa7286ea
SiamRahman29 <siam@graduate.utm.my>
2024-05-31 15:03:54 +0000

    Adding Allowed Host

--- comps/settings.py
+++ comps/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "54.254.211.186",
     "ec2-54-254-211-186.ap-southeast-1.compute.amazonaws.com",
-    "localhost"
+    "localhost",
+    "ec2-54-255-244-103.ap-southeast-1.compute.amazonaws.com"
 ]
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
